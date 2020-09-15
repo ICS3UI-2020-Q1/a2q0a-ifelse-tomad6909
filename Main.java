126a32022acd7c9b1700993ee6432510afd70719
tomad6909 <tomad6909@wrdsb.ca>
2020-09-15 13:30:28 +0000

    Finished A2Q0

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 /**
- *
+ * This program lets the user know if they can drive or not
  * @author 
  */
 public class Main {
@@ -9,7 +10,38 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for the user
+    Scanner input = new Scanner(System.in);
+
+    // define the minimum driving age 
+    final int MIN_DRIVER_AGE = 16; 
+    // define the minimun voting age
+    final int VOTING_AGE = 18;
+
+    // prompt the user for thier age
+    System.out.println("Please enter your age:");
     
+    int age = input.nextInt();
+
+    if(age >= VOTING_AGE){
+      System.out.println("You can vote");
+      System.out.println("You can legally drive");
+    } else if(age >= MIN_DRIVER_AGE){
+      // tell them they can drive
+      System.out.println("You can legally get your license");
+    } else {
+      // not old enough to drive
+      System.out.println("Sorry, you can't drive yet!");
+    }
+  
+
+
+
+    //boolean logical operators
+    //between age 18 and 30
+    if(age >= 18 && age <= 30){
+      System.out.println("Time to go to college");
+    }
+
   }
 }
